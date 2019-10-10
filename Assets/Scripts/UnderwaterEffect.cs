6944cbe3518b6db0a50d26496f4c4afd7df275c4
daniel-ilett <danielilett@gmail.com>
2019-10-10 23:50:55 +0100

    Converted to ScriptableObject system.
    Added a system to facilitate composite effects and converted individual effects to ScriptableObjects that can be individually customised.

---
+++ Assets/Scripts/UnderwaterEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Image Effects Ultra/Underwater", order = 1)]
+public class UnderwaterEffect : BaseEffect
+{
+    [SerializeField]
+    private float strength = 0.01f;
+
+    [SerializeField]
+    private Color waterColour = Color.white;
+
+    public override void Render(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, baseMaterial);
+    }
+}
