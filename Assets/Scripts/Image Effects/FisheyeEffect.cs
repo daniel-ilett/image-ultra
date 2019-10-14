16a5e950c2da263e91324289488609672b65baeb
Daniel Ilett <danielilett@gmail.com>
2019-10-14 15:24:45 +0100

    Modified Mosaic effect.
    Converted to the Scriptable Object system.

---
+++ Assets/Scripts/Image Effects/FisheyeEffect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Image Effects Ultra/Fisheye", order = 1)]
+public class FisheyeEffect : BaseEffect
+{
+    [SerializeField]
+    private float pow;
+
+    private void Update()
+    {
+        baseMaterial.SetFloat("_BarrelPower", pow);
+    }
+
+    public override void Render(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, baseMaterial);
+    }
+}
---
+++ Assets/Scripts/Image Effects/MosaicEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Image Effects Ultra/Mosaic", order = 1)]
+public class MosaicEffect : BaseEffect
+{
+    [SerializeField]
+    private int yTileCount = 100;
+
+    private int YTileCount
+    {
+        get
+        {
+            return yTileCount;
+        }
+        set
+        {
+            yTileCount = value;
+            baseMaterial.SetInt("_YTileCount", value);
+        }
+    }
+
+    public override void Render(RenderTexture src, RenderTexture dst)
+    {
+        RenderTexture tmp = 
+            RenderTexture.GetTemporary(yTileCount, (int)(((float)src.height / src.width) * yTileCount));
+
+        tmp.filterMode = FilterMode.Point;
+
+        Graphics.Blit(src, tmp);
+        Graphics.Blit(tmp, dst, baseMaterial);
+    }
+}
