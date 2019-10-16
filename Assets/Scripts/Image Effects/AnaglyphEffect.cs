ce09ef6b9bb06c0cfaae7817b371e25675b30cfc
daniel-ilett <danielilett@gmail.com>
2019-10-16 15:57:02 +0100

    Added Anaglyph script.

---
+++ Assets/Scripts/Image Effects/AnaglyphEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Image Effects Ultra/Anaglyph 3D", order = 1)]
+public class AnaglyphEffect : BaseEffect
+{
+    [SerializeField]
+    private float strength = 0.01f;
+
+    // Find the Anaglyph shader source.
+    public override void OnCreate()
+    {
+        baseMaterial = new Material(Resources.Load<Shader>("Shaders/Anaglyph3D"));
+
+        baseMaterial.SetFloat("_Strength", strength);
+    }
+
+    public override void Render(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, baseMaterial);
+    }
+}
