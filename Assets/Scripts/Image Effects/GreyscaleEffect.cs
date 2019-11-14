11919b9149fc14fd8b13031e29f204694910c8da
Daniel Ilett <danielilett@gmail.com>
2019-11-14 00:39:16 +0000

    Added Greyscale and composite filmic effects.

---
+++ Assets/Scripts/Image Effects/GreyscaleEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Image Effects Ultra/Greyscale", order = 1)]
+public class GreyscaleEffect : BaseEffect
+{
+    // Find the Greyscale shader source.
+    public override void OnCreate()
+    {
+        baseMaterial = new Material(Resources.Load<Shader>("Shaders/Greyscale"));
+    }
+
+    public override void Render(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, baseMaterial);
+    }
+}
